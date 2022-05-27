2dd8c2579bf930295459686cdd85028a2971fc66
Reuben Roshan <lelopopsinger@gmail.com>
2022-05-28 00:27:29 +0800

    Section 1 done

---
+++ Section_1/Hello World/Hello World/Program.cs
@@ -0,0 +1,21 @@
+﻿//Namespace called system
+//Has a bunch of different classes
+//eg. what is a Console
+using System;
+
+//creating our own library
+namespace Hello_World
+{
+    class Program
+    {
+        //Entry point of our program
+        //static -> dont need to create an object for this class
+        //void -> function doesnt return anything
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Reuben");
+            Console.Read();
+        }
+    }
+}
